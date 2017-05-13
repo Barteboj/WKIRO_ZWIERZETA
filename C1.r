cd5b70be3fd5c827287c41480aa61ae14f1ca136
Comandoraf <rafal.potoczek@gmail.com>
2017-05-13 12:57:22 +0200

    C1, C2, readAllImages translation

--- C1.r
+++ C1.r
@@ -1,125 +1,80 @@
-C1 <- function(stim,
-               filters,
-               fSiz,
-               c1SpaceSS,
-               c1ScaleSS,
-               c1OL,
-               INCLUDEBORDERS = 0) {
-  #TODO
-  #docs
+C1 = function(stim, filters, fSiz, c1SpaceSS, c1ScaleSS, c1OL, INCLUDEBORDERS = 0) {
+
+  library('matlab')
+  library('EBImage')
   
-  DEBUG <- FALSE
-  PRINT <- FALSE
+  source('unpadimage.r')
+  source('sumfilter.r')
+  source('conv2same.r')
+  source('maxfilter.r')
 
-  require('matlab')
-  require('EBImage')
-  if (DEBUG) {
-    debugSource('unpadimage.r')
-    debugSource('sumfilter.r')
-    debugSource('conv2same.r')
-    debugSource('maxfilter.r')
-  } else {
-    source('unpadimage.r')
-    source('sumfilter.r')
-    source('conv2same.r')
-    source('maxfilter.r')
-  }
-
-  USECONV2 <- 1
-  numScaleBands <- length(c1ScaleSS) - 1  #convention: last element in c1ScaleSS is max index + 1 
-  numScales <- tail(c1ScaleSS, n = 1) - 1
+  USECONV2 = 1
+  
+  numScaleBands = length(c1ScaleSS) - 1  #convention: last element in c1ScaleSS is max index + 1 
+  numScales = tail(c1ScaleSS, n = 1) - 1
   # last index in scaleSS contains scale index where next band would start, i.e., 1 after highest scale!!
-  numSimpleFilters <- floor(length(fSiz) / numScales)
-  ScalesInThisBand <- list()
+  numSimpleFilters = floor(length(fSiz) / numScales)
+  ScalesInThisBand = list()
   for (iBand in 1:numScaleBands) {
-    ScalesInThisBand[[iBand]] <- c1ScaleSS[iBand]:(c1ScaleSS[iBand + 1] - 1)
+    ScalesInThisBand[[iBand]] = c1ScaleSS[iBand]:(c1ScaleSS[iBand + 1] - 1)
   }
     
   ##Rebuild all filters (of all sizes)
   ##
-  nFilts <- length(fSiz)
-  sqfilter <- list()
+  nFilts = length(fSiz)
+  sqfilter = list()
   for (i in 1:nFilts) {
-    temp_m <- matrix(filters[1:(fSiz[i]^2), i], nrow = fSiz[i], ncol = fSiz[i])
+    temp = matrix(filters[1:(fSiz[i]^2), i], nrow = fSiz[i], ncol = fSiz[i])
     if (USECONV2) {
-      temp_m <- temp_m[fSiz[i]:1, fSiz[i]:1] #flip in order to use conv2 instead of imfilter (%bug_fix 6/28/2007);
+      temp = temp[fSiz[i]:1, fSiz[i]:1] #flip in order to use conv2 instead of imfilter (%bug_fix 6/28/2007);
     }
-    sqfilter[[i]] <- temp_m
+    sqfilter[[i]] = temp
   }
 
   ## Calculate all filter responses (s1)
   ##
-  sqim <- stim ^ 2
-  iUFilterIndex <- 0
+  sqim = stim ^ 2
+  iUFilterIndex = 0
   # precalculate the normalizations for the usable filter sizes
-  uFiltSizes <- sort(unique(fSiz))
-  s1Norm <- list()
-  remove_zeroes <- function(x) { if (x == 0) { x <- 1 } else { x <- x } }
   
-  #for performance measure
-  #tic <- Sys.time()
-  
+  uFiltSizes = unique(fSiz)
+  s1Norm = list()
   for (i in 1:length(uFiltSizes)) {
-    temp_m <- (sumfilter(sqim,(uFiltSizes[i] - 1) / 2)) ^ 0.5
-    #avoid divide by zero
-    s1Norm[[uFiltSizes[i]]] <- apply(temp_m, 1:2, remove_zeroes)
+    s1Norm[[uFiltSizes[i]]]= (sumfilter(sqim,(uFiltSizes[i]-1)/2))^0.5
+    s1Norm[[uFiltSizes[i]]] = s1Norm[[uFiltSizes[i]]] + !s1Norm[[uFiltSizes[i]]]
   }
   
-  #totaltimespectextractingPatches <- Sys.time() - tic
-  #t_str <- as.numeric(totaltimespectextractingPatches, units = "secs")
-  #print(paste('calc sumfilters takes ', t_str, ' seconds'))
-  
-  s1 <- list()
+  s1 = list()
   for (iBand in 1:numScaleBands) {
-    s1[[iBand]] <- list()
+    s1[[iBand]] = list()
     for (iScale in 1:length(ScalesInThisBand[[iBand]])) {
-      s1[[iBand]][[iScale]] <- list()
+      s1[[iBand]][[iScale]] = list()
       for (iFilt in 1:numSimpleFilters) {
-        s1[[iBand]][[iScale]][[iFilt]] <- list()
-        iUFilterIndex <- iUFilterIndex + 1
+        s1[[iBand]][[iScale]][[iFilt]] = list()
+        iUFilterIndex = iUFilterIndex + 1
         if (!USECONV2) {
-          i_stim <- t(stim)
-          i_stim <- Image(data = i_stim, dim(i_stim), colormode = 0)
-          f_img <- abs(filter2(i_stim, t(sqfilter[[iUFilterIndex]]), boundary = 'replicate'))
-          
-          if (PRINT) {
-            display(f_img)
-          }
-          f_img <- t(as.array(f_img))
-          
-          if(!INCLUDEBORDERS) {
-            f_img <- removeborders(f_img, fSiz[iUFilterIndex])
-          }
-          f_img <-  as.double(f_img) / s1Norm[[fSiz[iUFilterIndex]]]
-          s1[[iBand]][[iScale]][[iFilt]] <- f_img
-          
+          stim = Image(data = stim, dim(stim), colormode = 0)
+          s1_temp = abs(filter2(stim, sqfilter[[iUFilterIndex]], boundary = 'replicate'))
         } else { #not 100% compatible but 20% faster at least
-          f_img <- abs(conv2same(stim, sqfilter[[iUFilterIndex]]))
-          
-          if (PRINT) {
-            temp = Image(data = t(f_img), dim(t(f_img)), colormode = 0)
-            display(temp)
-          }
-          
-          if (!INCLUDEBORDERS) {
-            f_img <- removeborders(f_img, fSiz[iUFilterIndex])
-          }
-          f_img <- as.double(f_img) / s1Norm[[fSiz[iUFilterIndex]]]
-          s1[[iBand]][[iScale]][[iFilt]] <- f_img
+          s1_temp = abs(conv2same(stim, sqfilter[[iUFilterIndex]]))
         }
+        if (!INCLUDEBORDERS) {
+          s1_temp = removeborders(s1_temp, fSiz[iUFilterIndex])
+        }
+        s1[[iBand]][[iScale]][[iFilt]] = as.double(s1_temp) / s1Norm[[fSiz[iUFilterIndex]]]
       }
     }
   }
     
   ## Calculate local pooling (c1)
   ##
-  c1 <- list()
+  c1 = list()
   for (iBand in 1:numScaleBands) {
-    c1[[iBand]] <- list()
+    c1[[iBand]] = list()
     for (iFilt in 1:numSimpleFilters) {
-      c1[[iBand]][[iFilt]] <- zeros(size(s1[[iBand]][[1]][[iFilt]]))
+      c1[[iBand]][[iFilt]] = zeros(size(s1[[iBand]][[1]][[iFilt]]))
       for (iScale in 1:length(ScalesInThisBand[[iBand]])) {
-        c1[[iBand]][[iFilt]] <- pmax(c1[[iBand]][[iFilt]], s1[[iBand]][[iScale]][[iFilt]])
+        c1[[iBand]][[iFilt]] = pmax(c1[[iBand]][[iFilt]], s1[[iBand]][[iScale]][[iFilt]])
       }
     }
   }
@@ -128,42 +83,30 @@ C1 <- function(stim,
   ## 2) pool over local neighborhood
   ##
   for (iBand in 1:numScaleBands) {
-    poolRange <- c1SpaceSS[iBand]
+    poolRange = c1SpaceSS[iBand]
     for (iFilt in 1:numSimpleFilters) {
-      t <- c1[[iBand]]
-      t <- t[[iFilt]]
-      ret <- maxfilter(t, c(0, 0, poolRange - 1, poolRange - 1))
-      
-      if (PRINT) {
-        temp = Image(data = t(ret), dim(t(ret)), colormode = 0)
-        display(temp)
-      }
-      
-      c1[[iBand]][[iFilt]] <- ret
+      c1[[iBand]][[iFilt]] = maxfilter(c1[[iBand]][[iFilt]],c(0, 0, poolRange-1, poolRange-1));
     }
   }
     
   ##(3) subsample
   ##
   for (iBand in 1:numScaleBands) {
-    sSS <- ceil(c1SpaceSS[iBand] / c1OL)
-    T <- list()
+    sSS = ceil(c1SpaceSS[iBand] / c1OL)
+    T = list()
     for (iFilt in 1:numSimpleFilters) {
-      t <- c1[[iBand]][[iFilt]]
-      d <- dim(t)
-      sy <- seq(1, d[1], by = sSS)
-      sx <- seq(1, d[2], by = sSS)
-      t <- t[sy, sx]
-      T[[iFilt]] <- t
+      temp = c1[[iBand]][[iFilt]]
+      dims = dim(temp)
+      T[[iFilt]] = temp[seq(1, dims[1], by = sSS), seq(1, dims[2], by = sSS)]
     }
-    c1[[iBand]] <- T
+    c1[[iBand]] = T
   }
 
   return(list(c1, s1))
 }
 
-removeborders <- function(sin, siz) {
-  sin <- unpadimage(sin, c((siz + 1) / 2, (siz + 1) / 2, (siz - 1) / 2, (siz -1) / 2))
-  sin <- padarray(sin, c((siz + 1) / 2, (siz + 1) / 2), 0, 'pre')
-  sout <- padarray(sin, c((siz - 1) / 2, (siz - 1) / 2), 0, 'post')
+removeborders = function(sin, siz) {
+  sin = unpadimage(sin, c((siz + 1) / 2, (siz + 1) / 2, (siz - 1) / 2, (siz -1) / 2))
+  sin = padarray(sin, c((siz + 1) / 2, (siz + 1) / 2), 0, 'pre')
+  sout = padarray(sin, c((siz - 1) / 2, (siz - 1) / 2), 0, 'post')
 }
--- C2.r
+++ C2.r
@@ -1,59 +1,45 @@
-C2 <- function(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,s2Target,c1){
+C2 = function(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,s2Target,c1){
   
-  DEBUG <- TRUE
-  if (DEBUG) {
-    debugSource('WindowedPatchDistance.r')
-  } else {
-    source('WindowedPatchDistance.r')
-  }
+  source('WindowedPatchDistance.r')
   
-  s1 <- list()
+  s1 = list()
   
-  nargin = nargin <- length(as.list(match.call())) - 1
+  nargin = nargin = length(as.list(match.call())) - 1
   if (nargin < 8) {
-    C1_output <- C1(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL)
-    c1 <- C1_output[[1]]
-    s1 <- C1_output[[2]]
+    C1Value = C1(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL)
+    c1 = C1Value[[1]]
+    s1 = C1Value[[2]]
   }
-  nbands <- length(c1)
-  c1BandImage <- c1
-  nfilts <- length(c1[[1]])
-  n_rbf_centers <- size(s2Target)[2]
-  L <- size(s2Target)[1] / nfilts;
-  PatchSize <- c(L^.5,L^.5,nfilts)
   
-  s2 <- cell(n_rbf_centers, 1)
+  nbands = length(c1)
+  c1BandImage = c1
+  nfilts = length(c1[[1]])
+  n_rbf_centers = size(s2Target)[2]
+  L = size(s2Target)[1] / nfilts;
+  PatchSize = c(L^.5,L^.5,nfilts)
+  
+  s2 = cell(n_rbf_centers, 1)
   
   #Build s2:
   # for all prototypes in s2Target (RBF centers)
   # for all bands
   # calculate the image response
   for(iCenter in 1:n_rbf_centers) {
-    t <- s2Target[,iCenter]
-    Patch <- reshape(as.array(t), PatchSize)
-    s2[[iCenter]] <- cell(nbands, 1)
+    Patch = reshape(as.array(s2Target[,iCenter]), PatchSize)
+    s2[[iCenter]] = cell(nbands, 1)
     for(iBand in 1:nbands) {
-      t <- WindowedPatchDistance(c1BandImage[[iBand]],Patch)
-      s2[[iCenter]][[iBand]] <- t
+      s2[[iCenter]][[iBand]] = WindowedPatchDistance(c1BandImage[[iBand]],Patch)
     }
   }
   
   #Build c2:
   # calculate minimum distance (maximum stimulation) across position and scales
-  #c2 = inf(n_rbf_centers, 1)
-  c2 <- matrix(data = Inf, ncol = n_rbf_centers, nrow = 1)
+  c2 = matrix(data = Inf, ncol = n_rbf_centers, nrow = 1)
   for (iCenter in 1:n_rbf_centers) {
     for (iBand in 1:nbands) {
-      t <- s2[[iCenter]][[iBand]]
-      m <- min(as.vector(t))
-      m <- c(c2[iCenter], m)
-      c2[iCenter] <- min(m)
+      c2[iCenter] = min(c2[iCenter],min(min(s2[[iCenter]][[iBand]])));
     }
   }
   
-  print('C2 done')
-  
-  output = list(c2, s2, c1, s1)
-  
-  return(output)
+  return(list(c2, s2, c1, s1))
 }
