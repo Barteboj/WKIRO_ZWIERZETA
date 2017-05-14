cb3b5c6a49e2c426c62fc94a872df848a5852e23
Bartosz Bojda <barteboj@gmail.com>
2017-05-15 01:15:25 +0200

    Update extractC2forcell, extractRandC1Patches

--- ProjektR/extractC2forcell.r
+++ ProjektR/extractC2forcell.r
@@ -1,51 +1,42 @@
-extractC2forcell <- function(filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,cPatches,cImages,numPatchSizes){
+extractC2forcell = function(filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,cPatches,cImages,numPatchSizes){
   
-  require('matlab')
-  
-  DEBUG <- TRUE
-  if (DEBUG) {
-    debugSource('C2.r')
-  } else {
-    source('C2.r')
-  }
+  library('matlab')
+  source('C2.r')
   
   numPatchSizes = min(numPatchSizes,length(cPatches))
-  print("Done extrC2_1")
   for (i in 1:numPatchSizes){
-    siz <- size(cPatches[[i]])[1]
-    numpatch <- size(cPatches[[i]])[2]
-    siz <- sqrt(siz/4)
-    
+    siz = size(cPatches[[i]])[1]
+    numpatch = size(cPatches[[i]])[2]
+    siz = sqrt(siz/4)
     for(j in 1:numpatch){
-      tmp <- array(data = cPatches[[i]][,j], dim = c(siz, siz, 4))
-      tmp <- tmp[siz:1,siz:1,]
-      tmp <- as.vector(tmp)
-      cPatches[[i]][,j] <- tmp
+      tmp = array(data = cPatches[[i]][,j], dim = c(siz, siz, 4))
+      tmp = tmp[siz:1,siz:1,]
+      tmp = as.vector(tmp)
+      cPatches[[i]][,j] = tmp
     }
   }
   
-  mC2 <- NULL;
+  mC2 = NULL;
   
   for(i in 1:length(cImages)){
     print(i)
-    stim <- cImages[[i]]
-    img_siz <- size(stim)
-    c1 <- NULL
-    iC2 <- NULL
+    stim = cImages[[i]]
+    img_siz = size(stim)
+    c1 = NULL
+    iC2 = NULL
     for(j in 1:numPatchSizes){
       if(is.null(c1)){
-        c2_output <- C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,cPatches[[j]])
-        tmpC2 <- c2_output[[1]]
-        tmp <- c2_output[[2]]
-        c1 <- c2_output[[3]]
+        c2Result = C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,cPatches[[j]])
+        tmpC2 = c2Result[[1]]
+        tmp = c2Result[[2]]
+        c1 = c2Result[[3]]
       } else {
-        c2_output <- C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,cPatches[[j]],c1) 
-        tmpC2 <- c2_output[[1]]
+        c2Result = C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,cPatches[[j]],c1) 
+        tmpC2 = c2Result[[1]]
       }
-      iC2 <- t(t(c(iC2, tmpC2)))
+      iC2 = c(iC2, tmpC2)
     }
-    mC2 <- cbind(mC2, iC2)
+    mC2 = cbind(mC2, iC2)
   }
-  
   return(mC2)
 }
